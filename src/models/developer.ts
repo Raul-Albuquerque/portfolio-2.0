ddd04c1ce9e908f4bb6df23b0ba97d6a96027b7a
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-05-04 11:59:36 -0300

    feat: create developer interface

---
+++ src/models/developer.ts
@@ -0,0 +1,40 @@
+interface Developer {
+  full_name: string
+  stack_pt: string
+  stack_en: string
+  about1: string
+  about2: string
+  sobre1: string
+  sobre2: string
+  skills: string
+  resume_url: string
+  profile_picture: string
+  cv_url: string
+  whatsapp_url: string
+  linkedin_url: string
+  email_url: string
+  github_url: string
+}
+
+export const Raul: Developer = {
+  full_name: 'Raul Albuquerque',
+  stack_pt: 'Full Stack Developer',
+  stack_en: 'Desenvolvedor Full Stack',
+  sobre1:
+    'Engenheiro de Software em formação, com experiência em Front-End e crescente habilidade em Back-End. Especializado em tecnologias como HTML, CSS, JavaScript, TypeScript e Python, com foco em frameworks como Django, Next.JS, Flask e React.JS. Demonstro minha capacidade em projetos diversos, de sites institucionais a aplicações web complexas.',
+  sobre2:
+    'Proficiente em bancos de dados SQL e NoSQL, como PostgreSQL, MySQL e MongoDB, e vivência em Docker para garantir portabilidade e eficiência. Sou bastante comprometido com a aprendizagem contínua e contribuição significativa para equipes, oferecendo criatividade, resolução de problemas e qualidade de código.',
+  about1:
+    'Software engineer in progress, with experience in Front-End and growing proficiency in Back-End development. Specialized in technologies such as HTML, CSS, JavaScript, TypeScript, and Python, with a focus on frameworks like Django, Next.JS, Flask, and React.JS. I demonstrate my ability in diverse projects, ranging from institutional websites to complex web applications.',
+  about2:
+    'Proficient in SQL and NoSQL databases like PostgreSQL, MySQL and MongoDB, with experience in Docker to ensure portability and efficiency. I am highly committed to continuous learning and making significant contributions to teams, offering creativity, problem-solving skills, and code quality.',
+  skills:
+    'html,css,bootstrap,sass,js,ts,py,vue,react,redux,nextjs,styledcomponents,tailwind,nodejs,django,flask,postgres,mysql,mongodb,docker,git',
+  resume_url: 'https://github.com/Raul-Albuquerque',
+  profile_picture: '/static/images/raul.svg',
+  cv_url: 'https://github.com/Raul-Albuquerque',
+  whatsapp_url: 'https://github.com/Raul-Albuquerque',
+  linkedin_url: 'https://github.com/Raul-Albuquerque',
+  email_url: 'https://github.com/Raul-Albuquerque',
+  github_url: 'https://github.com/Raul-Albuquerque',
+}
