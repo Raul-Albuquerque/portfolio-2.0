19303a49d424f0c503abdd5b3695c910eb922d86
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-05-06 01:24:54 -0300

    refactor: update component about style

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -14,9 +14,13 @@ export const SectionAbout = styled.section`
 export const AboutP = styled.p`
   font-size: 16px;
   font-weight: 500;
-  line-height: 24px;
+  line-height: 28px;
   color: var(--color-gray);
   margin-bottom: 20px;
+
+  @media (max-width: 1024px) {
+    line-height: 24px;
+  }
 `
 
 export const SkillsAbout = styled.article`
